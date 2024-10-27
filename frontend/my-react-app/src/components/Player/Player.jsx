a184ecdeabaa8d891ed74d1ff447df03b48f1ebe
unknown <anilthapa@ku.edu>
2024-10-26 23:40:56 -0500

    add placeholder elements for player

--- frontend/my-react-app/src/components/Player/Player.jsx
+++ frontend/my-react-app/src/components/Player/Player.jsx
@@ -1,9 +1,21 @@
 import './Player.css'
 import PlayerControls from './PlayerControls'
 
+import defaultImage from '../../assets/default.png'
+
 function Player() {
     return (
       <div className="player">
+        <div className="player-info">
+          <img 
+            src={defaultImage}
+            className="album-cover"
+          />
+          <div className="song-info">
+            <h3 className="song-title">Song</h3>
+            <p className="artist-name">Artist name</p>
+          </div>
+        </div>
         <PlayerControls/>
       </div>
     );
