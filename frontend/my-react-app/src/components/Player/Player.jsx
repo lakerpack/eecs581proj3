64b8a4a4dd64b91e6722453f9cb2d78baaafc50d
unknown <anilthapa@ku.edu>
2024-10-27 21:26:57 -0500

    set up states for components

--- frontend/my-react-app/src/components/Player/Player.jsx
+++ frontend/my-react-app/src/components/Player/Player.jsx
@@ -3,12 +3,57 @@ import PlayerControls from './PlayerControls'
 import ProgressBar from './ProgressBar'
 import VolumeControl from './VolumeControl'
 
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 
 import defaultImage from '../../assets/default.png'
 
 function Player() {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [songData, setSongData] = useState(null);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const [volume, setVolume] = useState(1);
+
+  const audioRef = useRef(new Audio());
+  const intervalRef = useRef();
+
+  let apiUrl = 'http://127.0.0.1:5000/api/random_song';
+
+  useEffect(() => {
+    const fetchSong = async () => {
+      try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+          throw new Error('Failed to fetch song data');
+        }
+        const data = await response.json();
+        // console.log(data)
+        setSongData(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchSong();
+  }, []);
+
+
+  useEffect(() => {
+    const audio = audioRef.current;
+    const setAudioData = () => {
+      setDuration(audio.duration);
+      setCurrentTime(audio.currentTime);
+    };
+    const setTimeUpdate = () => {
+      setCurrentTime(audio.currentTime);
+    };
+    audio.addEventListener('loadeddata', setAudioData);
+    audio.addEventListener('timeupdate', setTimeUpdate);
+    return () => {
+      audio.removeEventListener('loadeddata', setAudioData);
+      audio.removeEventListener('timeupdate', setTimeUpdate);
+    };
+  }, []);
 
   return (
     <div className="player">
@@ -24,7 +69,7 @@ function Player() {
       </div>
       <div className='controls-container'>
         <ProgressBar isPlaying={isPlaying} />
-        <VolumeControl/>
+        <VolumeControl />
       </div>
       <PlayerControls isPlaying={isPlaying} setIsPlaying={setIsPlaying} />
     </div>
