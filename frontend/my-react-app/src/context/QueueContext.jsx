4b2bd5f44c0b69c4e419f3ae3e429423a0b0332e
unknown <anilthapa@ku.edu>
2024-11-09 20:48:38 -0600

    add comments to QueueContext

--- frontend/my-react-app/src/context/QueueContext.jsx
+++ frontend/my-react-app/src/context/QueueContext.jsx
@@ -2,10 +2,23 @@ import { createContext, useContext, useState, useCallback, useEffect } from 'rea
 
 const QueueContext = createContext();
 
+/*
+In order to manage states between the QueueManager and the Player components so any interactions between
+the QueueManager and the queue and then the Player and the queue are reflected accurately between the two.
+We share context so queue management gets centralized when before it was just in Player, and we can use QueueManager to modify
+the queue even as Player reads from it. 
+Manages the queue (current list of songs) and currentQueuePosition (current playing position).
+Player is intended to read the queue and position for its songs, whle the QueueManager displays and modifies the queue.
+*/
+
 export function QueueProvider({ children }) {
     const [queue, setQueue] = useState([]);
     const [currentQueuePosition, setCurrentQueuePosition] = useState(null);
 
+    /*
+    Retrieves the current queue from the backend using 'http://127.0.0.1:5000/api/queue'. 
+    Set initial position for the Player and updates queue data overall. Just meant to ensure a consistent state.
+    */
     const fetchQueue = async () => {
         try {
             const response = await fetch('http://127.0.0.1:5000/api/queue');
@@ -22,7 +35,10 @@ export function QueueProvider({ children }) {
         }
     };
 
-
+    /*
+    Moved from Player for consistency with QueueManager. Allows the player to navigate the queue without needing to
+    maintain the queue state. (Instead QueueManager will do the bulk of the work)
+    */
     const getNextInQueue = useCallback(() => {
         const currentIndex = queue.findIndex(song => song.position === currentQueuePosition);
         if (currentIndex !== -1 && currentIndex < queue.length - 1) {
@@ -31,20 +47,27 @@ export function QueueProvider({ children }) {
         return null;
     }, [queue, currentQueuePosition]);
 
-
+    /*
+    Determine if there's anything else in the queue 
+    */
     const hasNext = useCallback(() => {
         const currentIndex = queue.findIndex(song => song.position === currentQueuePosition);
         return currentIndex !== -1 && currentIndex < queue.length - 1;
     }, [queue, currentQueuePosition]);
 
 
+    /*
+    If the position has been initialized, retrieve current queue every time you move. 
+    */
     useEffect(() => {
         if (currentQueuePosition !== null) {
             fetchQueue();
         }
     }, [currentQueuePosition]);
 
-
+    /*
+    Whenever the queue position moves, make sure that the queue gets updated so everything before it gets cut off. 
+    */
     useEffect(() => {
         if (queue.length > 0 && currentQueuePosition !== null) {
             const updatedQueue = queue.filter(song => song.position >= currentQueuePosition);
@@ -54,7 +77,9 @@ export function QueueProvider({ children }) {
         }
     }, [currentQueuePosition, queue]);
 
-
+    /*
+    Let QueueManager update the queue with additions and also updates Player's available songs. 
+    */
     const addToQueue = async (songTitle) => {
         try {
             let nextPosition = 1;
@@ -63,13 +88,7 @@ export function QueueProvider({ children }) {
             }
     
             const response = await fetch('http://127.0.0.1:5000/api/add_to_queue', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ 
-                    song_name: songTitle
-                })
+                method: 'POST', headers: {'Content-Type': 'application/json',}, body: JSON.stringify({ song_name: songTitle})
             });
     
             if (!response.ok) {
@@ -82,12 +101,17 @@ export function QueueProvider({ children }) {
         }
     };
 
-
+    /*
+    Update position 
+    */
     const updateQueuePosition = (position) => {
         setCurrentQueuePosition(position);
     };
 
-
+    /*
+    Format the song data since the pathing can be wonky. Two endpoints serve the direct audio file and the direct art cover file.
+    Centralize prep so not too many reuses of the same code in Player. 
+    */
     const formatSongData = (songDetails) => {
         const filename = songDetails.path.split('\\').pop();
         const audioUrl = `http://127.0.0.1:5000/api/audio/${filename}`;
@@ -100,7 +124,10 @@ export function QueueProvider({ children }) {
         };
     };
 
-
+    /*
+    Get the current song from the song name. Used when we're iterating over the queue and we need to get
+    the song at the current index. We're provided with position and name from the queue, so we just use the name for this info. 
+    */
     const fetchSongDetails = async (songName) => {
         try {
             const response = await fetch(`http://127.0.0.1:5000/api/song/${encodeURIComponent(songName)}`);
@@ -115,7 +142,9 @@ export function QueueProvider({ children }) {
         }
     };
 
-
+    /*
+    Centralize prep for the Audio object. 
+    */
     const prepareSongForPlayback = async (songDetails) => {
         return new Promise((resolve) => {
             const audio = new Audio();
@@ -124,7 +153,9 @@ export function QueueProvider({ children }) {
         });
     };
 
-
+    /*
+    Getter for the formatted song data. 
+    */
     const getFormattedSongData = async (songName) => {
         const songDetails = await fetchSongDetails(songName);
         if (songDetails) {
@@ -136,7 +167,10 @@ export function QueueProvider({ children }) {
         return null;
     };
 
-
+    /*
+    Remove from the queue by sending the position. Endpoint takes in a position to remove,
+    the queue we have in context is made up of [(position ,song_name)...]
+    */
     const removeFromQueue = async (position) => {
         try {
             const response = await fetch('http://127.0.0.1:5000/api/remove_from_queue', {
@@ -153,7 +187,9 @@ export function QueueProvider({ children }) {
         }
     };
 
-
+    /*
+    Backtrack and get the previous song in the queue. 
+    */
     const getPreviousInQueue = () => {
         const currentIndex = queue.findIndex(song => song.position === currentQueuePosition);
         if (currentIndex > 0) {
@@ -163,13 +199,18 @@ export function QueueProvider({ children }) {
     };
 
 
-
+    /*
+    Determine if there is a previous element. 
+    */
     const hasPrevious = () => {
         const currentIndex = queue.findIndex(song => song.position === currentQueuePosition);
         return currentIndex > 0;
     };
 
-
+    /*
+    Values within the context. queue and currentQueuePosition are our values to manage
+    The rest are helper functions to deal with the data
+    */
     const value = {
         queue,
         currentQueuePosition,
@@ -185,7 +226,7 @@ export function QueueProvider({ children }) {
         removeFromQueue
     };
 
-    
+
     return (
         <QueueContext.Provider value={value}>{children}</QueueContext.Provider>
     );
